ace1c2156774ca73ec9c45bfac7d8d464cab9dcf
anies <arocnies@gmail.com>
2017-11-30 23:27:01 -0500

    Removed unused stuff from test.

--- src/test/kotlin/HttpEndpointTest.kt
+++ src/test/kotlin/HttpEndpointTest.kt
@@ -1,7 +1,5 @@
 import com.anies.kithub.http.HttpEndpoint
-import com.anies.kithub.http.async
 import com.anies.kithub.http.launch
-import kotlin.js.Promise
 import kotlin.test.Test
 
 class HttpEndpointTest {
@@ -11,14 +9,6 @@ class HttpEndpointTest {
     fun testSimpleGetRequest() = testSuspend("Simple GET request") {
         val result = pokeApi.request("GET", "pokemon/1")
         println(result)
-    }
-
-    @Test fun bar() = "SomeTestFoo"
-
-    @Test fun timing() {
-        (0..10).forEach {
-            println(it)
-        }
     }
 }
 
